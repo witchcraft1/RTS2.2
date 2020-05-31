aa85f12cd77376da02f36aa18be8735724b64b0a
bogdan <bogdanvolodym@ukr.net>
2020-05-31 18:47:57 +0300

    comparing DFT and FFT execution time

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Lab 2.2");
@@ -23,6 +29,21 @@ public class Main {
 
         double[] sygnals = garmonic.calculateSygnalsForResultingGarmonic();
         chart.addSeries("Fourier Function", i, garmonic.calculateFFT(sygnals));
+
+        List<Long> DFTTime = new ArrayList<>();
+        List<Long> FFTTime = new ArrayList<>();
+
+        for (int i1 = 0; i1 < 100; i1++) {
+            long time = System.currentTimeMillis();
+            garmonic.calculateFFT(sygnals);
+            FFTTime.add(System.currentTimeMillis() - time);
+
+            time = System.currentTimeMillis();
+            garmonic.calculateDFT(sygnals);
+            DFTTime.add(System.currentTimeMillis() - time);
+        }
+        System.out.println("Середній час виконання DFT: " + DFTTime.stream().mapToLong(x -> x).average().getAsDouble());
+        System.out.println("Середній час виконання FFT: " + FFTTime.stream().mapToLong(x->x).average().getAsDouble());
 
         new SwingWrapper<>(chart).displayChart();
     }
